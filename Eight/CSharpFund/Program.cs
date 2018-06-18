f3053e51ec0929d213808c5214ecde6b2d39d788
Katelynn Campbell <klyncamp23@gmail.com>
2018-06-18 17:57:48 -0400

    Udemy

---
+++ Eight/CSharpFund/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace CSharpFund
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new StringBuilder("Hello World");
+
+            builder.Append('-', 10);
+            builder.AppendLine();
+            builder.Append("Header");
+            builder.AppendLine();
+            builder.Append('-', 10);
+
+            builder.Replace('-', '+');
+
+            builder.Remove(0, 10);
+
+            builder.Insert(0, new string('-', 10));
+
+            Console.WriteLine(builder);
+
+            Console.WriteLine("First Char: " + builder[0]);
+
+        }
+    }
+}
